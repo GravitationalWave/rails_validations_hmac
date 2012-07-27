d37b9dd6f30b6761ba0a8e312ee6b3c0616acb31
mxrguspxrt <margus@tione.eu>
2012-07-27 20:21:33 +0300

    Tests and Libary working

--- spec/lib/active_model/validations/hmac_validator_spec.rb
+++ spec/lib/active_model/validations/hmac_validator_spec.rb
@@ -4,6 +4,11 @@ describe ActiveModel::Validations::HmacValidator do
 
   subject { described_class.new(attributes: {}) }
 
+  before do
+    Object.send(:remove_const, :Post) rescue false
+    load 'spec/dummy/app/models/post.rb'
+  end
+
   it "calls validate_each when hmac specified" do
     class Post
       validates :hmac, hmac: {key: :'api_user.secret', data: [:body, :title]}
@@ -54,16 +59,32 @@ describe ActiveModel::Validations::HmacValidator do
   describe "#validate_each" do
     context "valid hmac" do
       it "does not add error" do
-        @p = Post.new(hmac: 'a', title: 'a', body: 'a')
-        @p.valid?
+        class Post
+          validates :hmac, hmac: {key: :'api_user.secret', data: [:body, :title]}
+        end
+
+        @u = mock(:api_user)
+        @u.should_receive(:secret).and_return('Secret')
+
+        @p = Post.new(hmac: '30965a942b83bf362deb8b0834e28ccdf7db132a', title: 'Title', body: 'Body')
+        @p.should_receive(:api_user).and_return(@u)
+        @p.valid?.should eq true
       end
     end
     context "invalid hmac" do
       it "adds an error" do
+        class Post
+          validates :hmac, hmac: {key: :'api_user.secret', data: [:body, :title]}
+        end
+
+        @u = mock(:api_user)
+        @u.should_receive(:secret).and_return('Secret')
+
+        @p = Post.new(hmac: '30965a942b83bf362deb8b0834e28ccdf7db132', title: 'Title', body: 'Body')
+        @p.should_receive(:api_user).and_return(@u)
+        @p.valid?.should eq false
       end
     end
   end
-
-
 
 end
